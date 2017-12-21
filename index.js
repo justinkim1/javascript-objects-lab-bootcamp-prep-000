f60b6dd97168eb76730e565100d6eff99501acef
Justin K <justin-kim1@hotmail.com>
2017-12-21 07:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(obj, key){
   return recipes
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete recipes.prop
+  delete object.key
   return recipes
 }
