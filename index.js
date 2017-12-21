b961824267ef3176a212013099e18eba10e067c8
Justin K <justin-kim1@hotmail.com>
2017-12-21 07:31:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign(recipes, {prop2: 2})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes[prop] = 1
+  object[key] = value
   return recipes
  }
 
